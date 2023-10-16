c235b64c8480f0d98d153aebbd8f1f0373084e29
Lorl27 <100353961+Lorl27@users.noreply.github.com>
2023-10-16 04:42:29 -0300

    Update ej18ALGEBRA2.py

--- ej18ALGEBRA2.py
+++ ej18ALGEBRA2.py
@@ -1,11 +1,9 @@
 def permutacionUno(permu:list)->list:
     n=len(permu)
     l=[]
-    if n=1:
+    if n==1:
         l.apend(permu[0])
     else:
-        for x in range(2,len):
-            l.append(permu[x])
-return l
-
-print(permutacionUno([1,2,3,4,5,6,7,8,9]))
+        for x in range(2,n):
+            l.append(permu[x-1])
+    return l
